05d69589d614c9f78ea41643d66159aa89df27c0
Alberto Casanova <58746126+JudahAvadom@users.noreply.github.com>
2023-01-15 21:30:08 -0500

    Header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,7 @@
 import CTA from './CTA'
 import './Header.css'
+import HeaderSocials from './HeaderSocials'
+import PC from '../../Img/Me.jpeg'
 
 const Header = () => {
   return (
@@ -9,6 +11,11 @@ const Header = () => {
         <h1>Layla Mora</h1>
         <h5 className='text-light'>Reparacion de computadoras</h5>
         <CTA />
+        <div className='me'>
+          <img src={PC} alt="me" />
+        </div>
+        <HeaderSocials />
+        <a href="#contact" className='scroll__down'>IR ABAJO</a>
       </div>
     </header>
   )
---
+++ src/Components/Header/HeaderSocials.tsx
@@ -0,0 +1,11 @@
+import { BsFacebook } from "react-icons/bs";
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href="https://www.facebook.com/profile.php?id=100084885894901" target="_blank"><BsFacebook /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
