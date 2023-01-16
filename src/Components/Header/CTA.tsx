fbc05f3f51dbe4f571ec782b8dc2fe0fafe2a28d
Alberto Casanova <58746126+JudahAvadom@users.noreply.github.com>
2023-01-15 19:35:04 -0500

    CSS  Variables

---
+++ src/Components/Header/CTA.tsx
@@ -0,0 +1,11 @@
+
+const CTA = () => {
+    return (
+        <div className="cta">
+            <a href="#" className="btn">Descargar HV</a>
+            <a href="#contact" className="btn btn-primary">Contactame</a>
+        </div>
+    )
+}
+
+export default CTA
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,16 @@
+import CTA from './CTA'
 import './Header.css'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hola, Mi nombre es</h5>
+        <h1>Layla Mora</h1>
+        <h5 className='text-light'>Reparacion de computadoras</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
